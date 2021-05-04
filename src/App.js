9190fc19152f596407e3e9842d8d1a963a57ce6d
OmarMWarraich <earthling986@gmail.com>
2021-05-04 08:30:07 +0500

    UX optimization

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import * as React from "react";
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import { Admin, Resource} from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from './Users';
+import { PostList } from './posts'
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
 <Admin dataProvider={dataProvider}>
-<Resource name="posts" list={ListGuesser} />
+<Resource name="posts" list={PostList} />
     <Resource name="users" list={UserList} />
     </Admin>
 );
--- src/posts.js
+++ src/posts.js
@@ -1,14 +1,32 @@
 import  * as React from 'react';
-import { List, Datagrid, TextField, ReferenceField } from 'react-admin';
+import { List, Datagrid, TextField, ReferenceField, EditButton } from 'react-admin';
 
 export const PostList = props => (
     <List {...props}>
-        <Datagrid rowClick="edit">
-            <ReferenceField source="userId" reference="users"><TextField source="id" /></ReferenceField>
-            <TextField source="id" />
+        {/* <Datagrid rowClick="edit"> */}
+            <Datagrid>
+                <TextField source="id" />
+            <ReferenceField source="userId" reference="users">
+                {/* <TextField source="id" /> */}
+                <TextField source="name" />
+                </ReferenceField>
+            {/* <TextField source="id" /> */}
+            {/* <TextField source="name" /> */}
             <TextField source="title" />
-            <TextField source="body" />
+            {/* <TextField source="body" /> */}
+            <EditButton />
         </Datagrid>
     </List>
 );
 
+
+
+// The Reference Field fetches the reference data and passes
+// it as a record to its child component
+// Just like the list, reference comps responsible 4 fetching 
+// and preparing data and delegate rendering to their choldren
+
+
+// to finish the postlist, place the post id field as a 
+// 1st column, & remove da body field. UX demands datagrid 
+// shudnt contain large chunks of text.
