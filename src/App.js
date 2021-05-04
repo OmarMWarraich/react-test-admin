71be382817a2f7f0b900518438e88e3bf90fe42e
OmarMWarraich <earthling986@gmail.com>
2021-05-04 07:29:39 +0500

    Selecting Columns

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import * as React from "react";
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import { UserList } from './Users';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
 <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={ListGuesser} />
+    <Resource name="users" list={UserList} />
     </Admin>
 );
 export default App;
+
+
+
+// Resource name= users informs react-admin to fetch the users
+// record from the api. resource also tells react comps to use 
+// CRUD ops. 
+// list = {ListGuesser} prop means dat react-admin
+// shud use same to display the list of posts.
+
+//da ListGuesser comp isnt meant to be used in production
+// build custome react component listguesser dumps guess code  in console
+
