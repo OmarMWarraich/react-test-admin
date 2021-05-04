e58ff86032166357871064628455973c34f49287
OmarMWarraich <earthling986@gmail.com>
2021-05-04 09:44:44 +0500

    Using a Custom Homepage

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import { UserList } from './Users';
 import { PostList, PostEdit, PostCreate } from './posts'
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
-
+import Dashboard from './Dashboard';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
-<Admin dataProvider={dataProvider}>
+
+<Admin dashboard= {Dashboard} dataProvider={dataProvider}>
 <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
     <Resource name="users" list={UserList} icon={UserIcon}/>
     </Admin>
